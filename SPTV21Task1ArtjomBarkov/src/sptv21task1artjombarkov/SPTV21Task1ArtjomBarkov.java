dde97a252fc84c48e290af58adf8568b2d7d0781
ArtjomBarkov <112948103+ArtjomBarkov@users.noreply.github.com>
2022-09-27 09:13:48 +0300

    Add files via upload

---
+++ SPTV21Task1ArtjomBarkov/src/sptv21task1artjombarkov/SPTV21Task1ArtjomBarkov.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sptv21task1artjombarkov;
+
+/**
+ *
+ * @author user
+ */
+public class SPTV21Task1ArtjomBarkov {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print("Java Hello ");
+        // TODO code application logic here
+    }
+    
+}
